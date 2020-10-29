022f11a004cf0769ff145625eb0ce94d970d8fde
katerinbarrera21 <katerinbarrera21@gmail.com>
2020-10-29 08:01:47 -0500

    Confi

--- JS/buscar.js
+++ JS/buscar.js
@@ -1,30 +1,7 @@
-function getsource(id) {
-
-    url = "https://api.spoonacular.com/recipes/" + id + "/information?apikey=35a167c622244301a1ac41e0994a0a42";
-
-    document.getElementById('sourceLink').innerHTML = url;
-    document.getElementById('sourceLink').href = url;
-}
-
-
-
-
-
-function getrecepe3(q) {
-
-    url = "https://api.spoonacular.com/recipes/search?apikey=35a167c622244301a1ac41e0994a0a42$number=1&query=" + q;
-    url.results[0].title + "</h1><br><img src='" + url.baseUri + url.results[0].image + "' width = '400'/><br>ready in" + url.results[0].readyInMinutes + "minutes"
-}
-
-function mostrar(res) {
-    document.getElementById("ouput").innerHTML = "<h1>" + res.results[0].title + "</h1><br><img src='" + res.baseUri + res.results[0].image + "' width = '400'/><br>ready in" + res.results[0].readyInMinutes + "minutes"
-    getsource(res.results[0].id)
-}
-
-
-
 function getrecepe(q) {
-    url = "https://api.spoonacular.com/recipes/716429/information?apiKey=35a167c622244301a1ac41e0994a0a42&query=" + q;
+
+
+    url = "http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-29&sortBy=publishedAt&apiKey=d6edc64bac4547a794a7012896052c9a";
 
     let ajax;
 
@@ -34,7 +11,9 @@ function getrecepe(q) {
     } else {
         // code for IE6, IE5
         ajax = new ActiveXObject("Microsoft.XMLHTTP");
+        ajax = new ActiveXObject("Msxm12.XMLHTTP");
     }
+
 
 
     //document.getElementById("informacion").innerHTML = url;
@@ -43,69 +22,42 @@ function getrecepe(q) {
     ajax.onreadystatechange = function() {
         if (ajax.readyState === 4 && ajax.status === 200) {
             console.log(ajax.responseText);
-            let response = ajax.responseText;
-            let responseJSON = JSON.parse(response);
-            //document.getElementById("informacion").innerHTML = ajax.responseText;
-            //callback(responseJSON);
-            document.getElementById("informacion").innerHTML = responseJSON.get;
+            //let response = ajax.responseText;
+            let responseJSON = JSON.parse(this.responseText);
+            console.log(responseJSON);
 
+            for (let i = 0; i < responseJSON.articles.length; i++) {
+                //const element = array[i];
 
-        }
-    };
-
-    ajax.onload = function() {
-        const superHeroes = responseJSON;
-        populateHeader(superHeroes);
-        showHeroes(superHeroes);
-    };
-
-    function populateHeader(jsonObj) {
-        const myH1 = document.createElement('h1');
-        myH1.textContent = jsonObj['squadName'];
-        header.appendChild(myH1);
-
-        const myPara = document.createElement('p');
-        myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-        header.appendChild(myPara);
-    }
-
-    function showHeroes(jsonObj) {
-        const heroes = jsonObj['members'];
-
-        for (var i = 0; i < heroes.length; i++) {
-            const myArticle = document.createElement('article');
-            const myH2 = document.createElement('h2');
-            const myPara1 = document.createElement('p');
-            const myPara2 = document.createElement('p');
-            const myPara3 = document.createElement('p');
-            const myList = document.createElement('ul');
-
-            myH2.textContent = heroes[i].name;
-            myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-            myPara2.textContent = 'Age: ' + heroes[i].age;
-            myPara3.textContent = 'Superpowers:';
-
-            const superPowers = heroes[i].powers;
-            for (var j = 0; j < superPowers.length; j++) {
-                const listItem = document.createElement('li');
-                listItem.textContent = superPowers[j];
-                myList.appendChild(listItem);
+                console.log(responseJSON.articles[i].author);
+                console.log(responseJSON.articles[i].urlToImage);
+                document.getElementById("foto").innerHTML = "<img id='foto' class= 'img-fluid' width= '300' heigth='300' src='" + responseJSON.articles[i].urlToImage + "' >";
             }
 
-            myArticle.appendChild(myH2);
-            myArticle.appendChild(myPara1);
-            myArticle.appendChild(myPara2);
-            myArticle.appendChild(myPara3);
-            myArticle.appendChild(myList);
 
-            section.appendChild(myArticle);
+
+
+            document.getElementById("informacion").innerHTML = responseJSON.responseText;
+            //var data = JSON.parse(this.responseText);
+            //totalPaginas = data.results.length;
+            //totalPaginas /=5;
+            //console.log(totalPaginas +'<--');
+            //let parteEntera = parseInt(totalPaginas);
+            //let parteDecimal = 5*( totalPaginas - parteEntera)
+            //totalPaginas = Math.ceil(totalPaginas);
+            //
+            //callback(responseJSON);
+            //document.getElementById("informacion").innerHTML = "<h1>" + responseJSON.id + "<h1>" + "</h1><br><img src='" + responseJSON.baseUri + responseJSON.image + "' width = '400'/><br>ready in" + responseJSON.readyInMinutes + "minutes";
+            // document.getElementById("informacion").innerHTML =
+
+
+
         }
+
     }
-
-
-
-
 }
+
+
 
 
 
